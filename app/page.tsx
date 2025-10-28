3a142a02f344f7cf1ef11f4e62f230d4c35c6252
Arthur <artu.donascimento@gmail.com>
2025-10-28 17:08:10 -0300

    creating home page with interactive background

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,47 @@
-import Image from "next/image";
+"use client";
+
+import { Cal_Sans } from "next/font/google";
+import Particles from "@/components/Particles";
+import Link from "next/link";
+import { Separator } from "@/components/ui/separator";
+
+const calSans = Cal_Sans({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400"],
+});
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+    <div
+      className={`relative flex items-center justify-center w-screen h-screen bg-gradient-to-bl from-zinc-950 via-zinc-900 to-zinc-950 flex flex-col justify-center items-center ${calSans.className} antialiased`}
+    >
+      <div className="relative w-full h-screen">
+        <Particles
+          particleColors={["#ffffff", "#ffffff"]}
+          particleCount={700}
+          particleSpread={20}
+          speed={0.0001}
+          particleBaseSize={100}
+          moveParticlesOnHover={true}
+          alphaParticles={true}
+          disableRotation={false}
+          sizeRandomness={1}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+      </div>
+
+      <div className="absolute flex flex-col justify-center items-center space-y-6  ">
+        <div className="bg-transparent w-96 flex items-center justify-evenly gap-4  text-xl text-zinc-50 [&>a]:hover:underline [&>a]:hover:text-zinc-200  ">
+          <Link href={"/projects"}>projetos</Link>
+          <Link href={""}>sobre</Link>
+          <Link href={""}>contato</Link>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+        <h1 className="text-9xl font-bold text-zinc-50 text-shadow-md text-shadow-zinc-600/60">
+          oartuu
+        </h1>
+        <Separator className="bg-zinc-800" />
+        <p className="text-zinc-400">Atualmente trabalhando em ...</p>
+      </div>
     </div>
   );
 }
