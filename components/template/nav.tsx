77ad63e1757635a1b159ef842edc6711abca0b63
Arthur <artu.donascimento@gmail.com>
2025-10-29 01:38:20 -0300

    fixing nav

--- components/template/nav.tsx
+++ components/template/nav.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export function Nav() {
   return (
-    <div className="w-full h-16 bg-transparent flex items-center justify-between p-4 mt-4">
+    <div className="w-11/12 h-16 bg-transparent flex items-center justify-between p-4 mt-4">
       <div>
         <Link href={"/"}>
           <ArrowLeft className="text-zinc-50 hover:cursor-pointer" />
--- components/template/project-nav.tsx
+++ components/template/project-nav.tsx
@@ -3,14 +3,14 @@ import Link from "next/link";
 
 export function ProjectNav() {
   return (
-    <div className="w-full h-16 bg-transparent flex items-center justify-between p-4 mt-4 ">
+    <div className="w-11/12 h-16 bg-transparent flex items-center justify-between p-4 mt-4 ">
       <div>
         <Link href={"/projects"}>
           <ArrowLeft className="text-zinc-50 hover:text-zinc-200 hover:cursor-pointer" />
         </Link>
       </div>
       <div className="px-4 mr-4">
-        <Link href={"https://github.com/oartuu"}>
+        <Link href={"https://github.com/oartuu"} rel="noopener" target="blank">
           <Github className="text-zinc-50 hover:text-zinc-200 hover:cursor-pointer" />
         </Link>
       </div>
